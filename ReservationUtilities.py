76f76c60bf3bdf264456a34dc712dc6949713eca
Tanner-Gladson <tannergladson1@gmail.com>
2022-06-26 16:56:27 -0400

    Created TimeUtilities, refactored Res to use TimeRange

--- ReservationUtilities.py
+++ ReservationUtilities.py
@@ -5,37 +5,73 @@ Created on Fri Jun 24 17:27:21 2022
 @author: tanne
 """
 
+from TimeUtilities import TimeRange
 
 class Res():
     '''
+    Reservation class, shortened to Res
+    
     Attributes
     ----------
-    start_time : tuple
-        start time of reservation, (mo, day, hour)
-    end_time : tuple
-        end time of reservation, (mo, day, hour) .
-    owner : string
-        Username of Res owner/creator.
     ID : string
         unique identification of reservation.
+    owner : string
+        Username of Res owner/creator.
+    day : int
+        Day (1-365) of the reservation.
+    tRange : TimeRange
+        Time range of reservation (attributes start & end)
+    start : float
+        Start time
+    end : float
+        End time
     active : Boolean, optional
         False if the reservation has been cancelled. The default is True.
+        
+    Methods
+    --------
+    __str__ : overloads 
 
     '''
 
-    def __init__(self, ID, owner, start_time, end_time, active=True):
-        self.start_time = start_time
-        self.end_time = end_time
+    def __init__(self, ID, owner, day, time_range=None, active=True, start_time=None, end_time=None):
+        '''
+        Construct Res (reservation) instance
+
+        Parameters
+        ----------
+        ID : str
+        owner : str
+        day : int
+        time_range : TimeRange, optional
+        
+            start_time : float, optional
+            end_time : float, optional
+        
+        active : bool, optional
+        '''
+        
+        # Establish time range value. Add 'start' and 'end' attributes for
+        #     convenience
+        if time_range != None:
+            self.tRange = time_range
+        else:
+            self.tRange = TimeRange(start=start_time, end=end_time)
+        
+        self.start = self.tRange.start
+        self.end = self.tRange.end
+        
+        self.day = day
         self.owner = owner
         self.ID = ID
         self.active = active
     
     
     def __str__(self):
-        return f'*Reservation*\nID:     {self.ID}\nOwner:  {self.owner}\nStart:  {self.start_time}\nEnd:    {self.end_time}\nActive? {self.active}\n'
+        return f'*Reservation*\nID:     {self.ID}\nOwner:  {self.owner}\nDay:    {self.day}\nTime:   {self.tRange}\nActive? {self.active}\n'
 
     def toString(self):
-        return f'ID:     {self.ID}\nOwner:  {self.owner}\nStart:  {self.start_time}\nEnd:    {self.end_time}\nActive? {self.active}'
+        return f'ID:     {self.ID}\nOwner:  {self.owner}\nDay:    {self.day}\nTimes:  {self.tRange.toString()}\nActive: {self.active}'
 
 
 class Writer():
@@ -53,7 +89,7 @@ class Writer():
     '''
     @staticmethod
     def write(c_res):
-        fileName = f'{c_res.ID}.txt'
+        fileName = f'reservations/{c_res.ID}.txt'
 
         with open(fileName, mode='w') as file:
             file.write(c_res.toString())
@@ -95,27 +131,31 @@ class Reader():
         '''
 
         # Read file
-        with open(f'{ID}.txt') as file:
+        with open(f'reservations/{ID}.txt') as file:
             lines = file.read().split('\n')
 
         # Go through each line and split key from values.
-        # Clean up data for conversion
+        # Clean up data for conversion. Gives 2d list of strings: 
+        # [[key, val], ...]
         for i in range(len(lines)):
             lines[i] = lines[i].split(':')
             for j in range(len(lines[i])):
-                lines[i][j] = lines[i][j].strip()
+                lines[i][j] = lines[i][j].strip().lower()
+        
 
-
-        # Create dictionary of constructors to clean up data.
-        constructor = {'ID': str,
-                 'start_time': Reader.__tuple_floats,
-                 'end_time': Reader.__tuple_floats,
+        # Create dictionary of classes/constructors to clean up data.
+        # 1) converts each value to correct type
+        # 2) places each value in a new list, in ordered positions
+        
+        constructor = {'id': str,
+                 'day': int,
+                 'times': TimeRange,
                  'owner': str,
                  'active': bool}
 
-        positions = {'ID': 0,
-                 'start_time': 2,
-                 'end_time': 3,
+        positions = {'id': 0,
+                 'day': 2,
+                 'times': 3,
                  'owner': 1,
                  'active': 4}
 
@@ -124,30 +164,10 @@ class Reader():
         for line in lines:
             key = line[0]
             data[positions[key]] = constructor[key](line[1])
-
+            
         return data
+        
 
-    @staticmethod
-    def __tuple_floats(rawTuple):
-        '''
-        Takes a tuple-like string, and converts into tuple with d_type = float
-
-        Returns
-        -------
-        Tuple of floats
-
-        '''
-        clean_string = rawTuple.strip().replace('(','').replace(')','')
-        myList = clean_string.split(',')
-
-        for i in range(len(myList)):
-            try:
-                myList[i] = float(myList[i])
-
-            except ValueError:
-                raise ValueError(f'Could not convert {myList} into tuple of floats')
-
-        return tuple(myList)
 
 if __name__ == '__main__':
     c_res = Reader.read('testRes')
@@ -155,3 +175,5 @@ if __name__ == '__main__':
     print(c_res)
     
     Writer.write(c_res)
+    
+    
---
+++ TimeUtilities.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 26 16:46:55 2022
+
+@author: tanne
+"""
+
+class TimeTools():
+    '''
+    Static class for manipulating time values
+    '''
+    
+    @staticmethod
+    def float_to_string(time):
+        '''
+        Converts the float to standard time (string, eg '12:00 to 13:00')
+        '''
+        hours = int(time//1)
+        minutes = str(int((time % 1) * 60)).rjust(2, '0')
+        return f'{hours}:{minutes}'
+    
+    
+class TimeRange():
+    '''
+    Attributes
+    ----------
+    start_h : float
+        start hour of time range
+    end_time : float
+        start hour of time range
+        
+    Methods
+    ---------
+    __str__
+        overload to print in readable fashion
+    
+    toString
+        convert end & start floats to tuple-like string 
+        
+    __fromString : private, static method
+        convert tuple-like string to tuple of floats
+        
+    __contains__ : overloading operator
+        Returns true if other can fit within self
+    '''
+    
+    def __init__(self, string=None, start=None, end=None):
+        '''
+        Constructs time range object from start & end, or from string
+
+        Parameters
+        ----------
+        start : float, optional
+            Start time
+        end : float, optional
+            End time
+        string : string, optional
+            Tuple-like string of end and start time. The default is False.
+
+        '''
+        # Call fromString to assign start & end times
+        
+        
+        if string != None:
+            self.start, self.end = TimeRange.__fromString(string)
+            
+        else:
+            self.start = float(start)
+            self.end = float(end)
+    
+    def __contains__(self, other):
+        '''
+        For 'range1 in range 2', returns True if range1 can fit within range2
+            (that is: other in self). Else false
+            
+        Parameters
+        ------------
+        self : TimeRange
+        other : TimeRange
+        '''
+        can_fit = False
+        
+        if other.start >= self.start and other.end <= self.end:
+            can_fit = True
+        
+        return can_fit
+    
+    
+    def __str__(self):
+        '''
+        Overload string operator to print out readable version of TimeRange. 
+        Utilizes TimeTools
+        '''
+        s = TimeTools.float_to_string(self.start)
+        e = TimeTools.float_to_string(self.end)
+        return f'{s} to {e}'
+    
+    def toString(self):
+        '''
+        Condenses object's data into form suitable for writing to file
+        '''
+        return str((self.start, self.end))
+    
+    @staticmethod
+    def __fromString(string):
+        '''
+        Returns a tuple of floats from a similar-looking string
+        '''
+        clean_string = string.strip().replace('(','').replace(')','')
+        myList = clean_string.split(',')
+
+        for i in range(len(myList)):
+            try:
+                myList[i] = float(myList[i])
+
+            except ValueError:
+                raise ValueError(f'Could not convert {string} into tuple of floats')
+
+        return tuple(myList)
